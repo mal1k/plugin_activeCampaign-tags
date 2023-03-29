43682a4812ca0b77a5ccc6035c02040f68b8dc26
mal1k <00154@ukr.net>
2023-03-29 20:28:02 +0300

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package ActiveCampaign Tags
+ * @version 1.0.0
+ */
+/*
+Plugin Name: ActiveCampaign Tags
+Description: This plugin is used to synchronize tags in WordPress and ActiveCampaign
+Author: Alex Kotlyarsky
+Version: 1.0.0
+Author URI: http://t.me/alex_mal1k
+*/
