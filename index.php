788d58f151295eb5b5b73098753ba2361170d7dd
mal1k <00154@ukr.net>
2023-03-30 17:14:23 +0300

    Added schedule task

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: ActiveCampaign Tags
 Description: This plugin is used to synchronize tags in WordPress and ActiveCampaign
-Author: Alex Kotlyarsky
+Author: </Alex-K>
 Version: 1.0.0
 Author URI: http://t.me/alex_mal1k
 */
@@ -21,35 +21,35 @@ include_once 'Class/ActiveCampaignTags.php';
  * @return void
  */
 function save_post_action($post_id){
-    if ( ! wp_is_post_autosave( $post_id ) && ! wp_is_post_revision( $post_id ) ) {
-        $activeCampaignTags = new ActiveCampaignTags();
-        $subscribers = getSubscriberEmailsOfProductID($post_id);
-
-        // get the list of tags associated with the post
-        $tags = wp_get_post_tags($post_id, array('fields' => 'names'));
-
-        foreach ($subscribers as $subscriber)
-            $activeCampaignTags->addUserToArrayByEmail($subscriber);
-
-        print_r($activeCampaignTags->getUsersArray());
-
-        foreach ($tags as $tag)
-            $tagsInActiveCampaign[] = $activeCampaignTags->getTagIdByName($tag);
-
-        
-
-        print_r($tagsInActiveCampaign);
-        
-        die();
-
-        print_r( $activeCampaignTags->getTagIdByName('Alerts') );
-        die();
-    }
+    if ( ! wp_is_post_autosave( $post_id ) && ! wp_is_post_revision( $post_id ) )
+        wp_schedule_single_event(time() + 30, 'schedule_handler', array($post_id));
 }
-add_action( 'save_post', 'save_post_action', 10, 3 );
+add_action('save_post_product', 'save_post_action', 10, 3 );
 
-// достать пользователей которые имеют подписку на продукт
+// Define a handler function.
+function schedule_event_tags_handler($post_id) {
+    $activeCampaignTags = new ActiveCampaignTags();
+    $subscribers = getSubscriberEmailsOfProductID($post_id);
+    $tagsInActiveCampaign = array();
+
+    // get the list of tags associated with the post
+    $tags = wp_get_post_tags($post_id, array('fields' => 'names'));
+
+    foreach ($subscribers as $subscriber)
+        $activeCampaignTags->addUserToArrayByEmail($subscriber);
+
+    foreach ($tags as $tag)
+        $tagsInActiveCampaign[] = $activeCampaignTags->getTagIdByName($tag);
+
+    $activeCampaignTags->addTagsToUsers(array_unique($tagsInActiveCampaign), $activeCampaignTags->getUsersArray());
+}
+add_action('schedule_handler', 'schedule_event_tags_handler', 10, 2);
+
+// get users who have a subscription to the product
 function getSubscriberEmailsOfProductID($product_id)
 {
-    return ["testemail@gmail.com", "test@gmail.com", "00154@ukr.net"];
+    return [
+        "valeriy.env@gmail.com",
+        "testemail@gmail.com"
+    ];
 }
